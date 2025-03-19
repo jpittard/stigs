e87977be7570d93cbbe61d61739cb7b91d11a74e
james.m.pittard <james.pittard@dsainc.com>
2025-03-19 14:34:40 -0400

    reporting opens by cat level

--- v2/comment.py
+++ v2/comment.py
@@ -1,24 +1,60 @@
 import xml.etree.ElementTree as ET
 import os
+import datetime
+import re
 
 
-COMMENT = 'Darrell Baldridge validated on 09/20/2024 that the finding is'
+def get_formatted_date():
+    today = datetime.date.today()
+    return f'{today.month:02d}/{today.day:02d}/{today.year}'
+
+FORMATTED_DATE = get_formatted_date()
+FINDING_STMT = f'Erik Jensen validated on {FORMATTED_DATE} that the finding is'
 FILENAME_EXTENSION = 'ckl'
 FILENAME_PARTICLE = '_jp'
+AC2SP_SEPERATOR = '******** AC2SP Notations ********'
+
+def get_status_text(status):
+    if status == 'NotAFinding':
+        return 'NOT A FINDING'
+    elif status == 'Not_Applicable':
+        return 'NOT APPLICABLE'
+    elif status == 'Open':
+        return 'OPEN'
+    elif status == 'Not_Reviewed':
+        return 'NOT REVIEWED'
+    else:
+        raise ValueError(f'Unknown status: {status}')
+
 
 def prepend_comment(tree):
+    '''Add a duplicate of the latest comment with an updated finding statement.
+    This is useful if not much has changed.'''
     for vuln in tree.getroot().findall('STIGS/iSTIG/VULN'):
         status = vuln.findall('STATUS')[0].text
-        english_status = 'OPEN'
-        if status == 'NotAFinding':
-            english_status = 'NOT A FINDING'
-        elif status == 'Not_Applicable':
-            english_status = 'NOT APPLICABLE'
-        
+        english_status = get_status_text(status)     
 
-        comment_node = vuln.findall('COMMENTS')[0]
+        comment_node = vuln.find('COMMENTS')
         latest_comment = get_latest_comment(comment_node.text)
-        revised = f'{COMMENT} {english_status}.\n{latest_comment}\n{comment_node.text}'
+        revised = f'{FINDING_STMT} {english_status}.\n{latest_comment}\n{comment_node.text}'
+        comment_node.text = revised
+    return tree
+
+
+def prepend_finding_statement(tree):
+    '''Add finding statement to file with no previous finding statements'''
+    for vuln in tree.getroot().findall('STIGS/iSTIG/VULN'):
+        comment_node = vuln.find('COMMENTS')
+        vuln_id = get_stig_data(vuln, 'Vuln_Num')
+        status = vuln.find('STATUS').text
+        english_status = get_status_text(status)
+        if comment_node is None or comment_node.text is None:
+            print(f'No comment for {vuln_id}')
+            revised = f'{FINDING_STMT} {english_status}.'
+        else:
+            lines = comment_node.text.split('\n')
+            noblanks = [line for line in lines if line and line.strip()]
+            revised = f'{FINDING_STMT} {english_status}.\n{'\n'.join(noblanks)}'
         comment_node.text = revised
     return tree
 
@@ -38,20 +74,100 @@ def get_latest_comment(text):
     return '\n'.join(lines[1:pos])
 
 
+def get_stig_data(vuln, key):
+    return vuln.find(f'STIG_DATA[VULN_ATTRIBUTE="{key}"]/ATTRIBUTE_DATA').text
+
+
+def update_latest_comment(tree):
+    '''Update the date in first line to current date'''
+    date_pattern_1 = r'\d{2}/\d{2}/\d{4}'
+    date_pattern_2 = r'\d{4}/\d{2}/\d{2}'
+    for vuln in tree.getroot().findall('STIGS/iSTIG/VULN'):
+        status = vuln.findall('STATUS')[0].text
+        vuln_id = get_stig_data(vuln, 'Vuln_Num')   
+        english_status = get_status_text(status)
+        if english_status == 'NOT REVIEWED':
+            print(f'  {vuln_id} not reviewed (skipping)')
+            continue
+     
+        comment_node = vuln.findall('COMMENTS')[0]
+        if comment_node is None or comment_node.text is None:
+            raise ValueError(f'No comment found for {vuln_id}')
+
+        lines = comment_node.text.split('\n')
+        # Remove blank lines from top
+        nonblank_lines = []
+        first_line_found = False
+        for line in lines:
+            if line is None or line.strip() == '' or first_line_found:
+                continue
+            else:
+                first_line_found = True
+                nonblank_lines.append(line)
+
+        if len(nonblank_lines) < 1 or len(nonblank_lines[0]) == 0:
+            raise ValueError(f'No comment text found for {vuln_id}')
+        top_line = nonblank_lines[0]
+        # Check that status is reflected in top line
+        if english_status not in top_line:
+            raise ValueError(f'Status {english_status} not found in first line for {vuln_id}')
+        # Update date to current date
+        if re.search(date_pattern_1, top_line):
+            nonblank_lines[0] = re.sub(date_pattern_1, FORMATTED_DATE, top_line)
+        elif re.search(date_pattern_2, top_line):
+            nonblank_lines[0] = re.sub(date_pattern_2, FORMATTED_DATE, top_line)
+        else:
+            raise ValueError(f'No date found in first line for {vuln_id}')
+
+        comment_node.text = '\n'.join(nonblank_lines)
+    return tree
+
+
+def combine_rhel_ami_ckl(ac2sp_ckl, rhel_ckl):
+    ac2sp_vulns = map()
+    for vuln in ac2sp_ckl.getroot().findall('STIGS/iSTIG/VULN'):
+        vuln_id = get_stig_data(vuln, 'Vuln_Num')
+        ac2sp_vulns[vuln_id] = vuln.find('COMMENTS').text
+
+    for vuln in rhel_ckl.getroot().findall('STIGS/iSTIG/VULN'):
+        vuln_id = get_stig_data(vuln, 'Vuln_Num')
+        comment_node = vuln.find('COMMENTS')
+        comment_node.text = f'{comment_node.text}\n\n{AC2SP_SEPERATOR}\n{ac2sp_vulns[vuln_id]}'
+    
+    return rhel_ckl
+
+
 def write_revised_file(filename, fcn):
-    tree = ET.parse(filename) 
-    revised_tree = fcn(tree)
+    try:
+        tree = ET.parse(filename)
+        revised_tree = fcn(tree)
+    except (ET.ParseError, ValueError) as e:
+        print(f'  ERROR: {filename}:\n  {str(e)}')
+        return
+    
     new_filename = os.path.splitext(os.path.basename(filename))[0] + f'{FILENAME_PARTICLE}.{FILENAME_EXTENSION}'
     print(f'Writing {new_filename}')
     ET.indent(tree, space='\t', level=0)
     revised_tree.write(new_filename, encoding='utf-8', xml_declaration=True)
 
 
-def write_all(directory):
-    for filename in os.listdir(directory):
-        if filename.endswith('.ckl') and not filename.endswith(f'{FILENAME_PARTICLE}.{FILENAME_EXTENSION}'):
-            write_revised_file(f'{directory}/{filename}')
+def write_all(dirname, fcn):
+    if os.path.isdir(dirname):
+        print(f'Processing directory {dirname}')
+        for filename in os.listdir(dirname):
+            if not filename.endswith('.' + FILENAME_EXTENSION) or filename.endswith(f'{FILENAME_PARTICLE}.{FILENAME_EXTENSION}'):
+                print(f'Skipping file {filename}')
+                continue
+            write_revised_file(f'{dirname}/{filename}', fcn)
+    else:
+        print(f'Processing file {dirname}')
+        write_revised_file(dirname, fcn)
+    print('Done')
 
 
-write_all('.')
+# write_all('../ckl/CUI_NA2P_connected_RHEL_7_v3r14_20240327_ami.ckl', update_latest_comment)
+write_all('../ckl/i2ar-test-jboss-web-0-ej_rhel8_V2R2_20250214.ckl', prepend_finding_statement)
+
+
+
 
--- v2/report_v2.py
+++ v2/report_v2.py
@@ -1,17 +1,25 @@
 import xml.etree.ElementTree as ET
 import csv
 import os
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, field
+from typing import Callable
 
 CHECKLIST_EXTENSION = 'ckl'
+SEVERITIES = {'low': 'CAT I', 'medium': 'CAT II', 'high': 'CAT III'}
+CAT_LEVELS = {'CAT III': '3', 'CAT II': '2', 'CAT I': '1'}
+FIELDLIST = ['severity', 'vuln_id', 'rule_ver', 'file', 'rule_title', 'comments', 'finding_details']
+REPORT_NAME = 'report.csv'
 
 
-@dataclass
+@dataclass(order=True, frozen=True)
 class Vuln:
+    sort_index: int = field(init=False, repr=False)
+    file: str
     vuln_id: str
     rule_id: str
     severity: str
     group_title: str
+    rule_ver: str
     rule_title: str
     discussion: str
     check_content: str
@@ -20,57 +28,99 @@ class Vuln:
     status: str
     comments: str
     finding_details: str
+    def __post_init__(self):
+        object.__setattr__(self, 'sort_index', CAT_LEVELS[self.severity] + self.rule_ver)
 
 
 
-def get_vulnerabilities(tree):
+def get_vulnerabilities(tree: ET, filename: str) -> list[Vuln]:
     vulns: list[Vuln] = []
     for vuln in tree.getroot().findall('STIGS/iSTIG/VULN'):
         v = Vuln(
+            file=os.path.basename(filename),
             vuln_id=get_stig_data(vuln, 'Vuln_Num'),
             rule_id=get_stig_data(vuln, 'Rule_ID'),
-            severity=get_stig_data(vuln, 'Severity'),
+            severity=SEVERITIES[get_stig_data(vuln, 'Severity')],
             group_title=get_stig_data(vuln, 'Group_Title'),
+            rule_ver=get_stig_data(vuln, 'Rule_Ver'),
             rule_title=get_stig_data(vuln, 'Rule_Title'),
-            discussion=get_stig_data(vuln, 'Discussion'),
+            discussion=get_stig_data(vuln, 'Vuln_Discuss'),
             check_content=get_stig_data(vuln, 'Check_Content'),
             fix_text=get_stig_data(vuln, 'Fix_Text'),
             cci=get_stig_data(vuln, 'CCI_REF'),
-            status=vuln.find('STATUS'),
-            comments=vuln.find('FINDING_DETAILS'),
-            finding_details=vuln.find('COMMENTS')
+            status=get_element(vuln, 'STATUS'),
+            comments=get_element(vuln, 'FINDING_DETAILS'),
+            finding_details=get_element(vuln, 'COMMENTS')
             )
         vulns.append(v)
     return vulns
 
 
-def write_csv(vulns):
-    with open('vulns.csv', 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=asdict(vulns[0]).keys())
+def write_csv(vulns: list[Vuln]) -> None:
+    print(f'Writing {len(vulns)} vulnerabilities to {REPORT_NAME}')
+    
+    with open(REPORT_NAME, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=FIELDLIST)
         writer.writeheader()
         for vuln in vulns:
-            writer.writerow(asdict(vuln))
+            fields_to_print = {key: asdict(vuln).get(key) for key in FIELDLIST}
+            writer.writerow(fields_to_print)
 
 
-def get_stig_data(vuln, key):
-    return vuln.find(f'STIG_DATA[VULN_ATTRIBUTE="{key}"]/ATTRIBUTE_DATA').text
+def get_stig_data(vuln: Vuln, key: str) -> str:
+    attr = vuln.find(f'STIG_DATA[VULN_ATTRIBUTE="{key}"]/ATTRIBUTE_DATA')
+    if attr is None:
+        print(f'No key found for \'{key}\'')
+        return ''
+    return attr.text
+
+def get_element(vuln: Vuln, key: str) -> str:
+    attr = vuln.find(key)
+    if attr is None:
+        print(f'No key found for \'{key}\'')
+        return ''
+    return attr.text
+
+def get_filtered_vulns(filename: str, filter_fcn: Callable) -> list[Vuln]:
+    tree = ET.parse(f'{filename}')
+    vulns = get_vulnerabilities(tree, filename)
+    found = filter_fcn(vulns)
+    print(f'{filename}: {len(found)}')
+    return found
 
 
-def report(directory, fcn):
-    total = 0
-    for filename in os.listdir(directory):
-        if filename.endswith('.ckl'):
-            tree = ET.parse(f'{directory}/{filename}')
-            vulns = get_vulnerabilities(tree)
-            found = fcn(vulns)
-            total += len(found)
-            print(f'{directory}/{filename}: {len(found)}')
-            write_csv(f'{directory}/{filename}', found)
-    print(f'Total: {total}')
+def create_vuln_filter(conditions: list[Callable]) -> Callable:
+    def filter_vulns(vulns):
+        return [v for v in vulns 
+               if all(condition(v) for condition in conditions)]
+    return filter_vulns
 
 
-def contains_fips(vulns):
-    return [v for v in vulns if 'FIPS' in v.check_content]
+def is_open(v: Vuln) -> bool:
+    return v.status == 'Open'
 
 
-report('.', "contains_fips")
+def report(dirname: str, conditions: list[Callable]) -> None:
+    '''Run report against file or directory of checklists using list of vulnerability criteria stated as functions'''
+    filter = create_vuln_filter(conditions)
+    vulns = []
+    if os.path.isdir(dirname):
+        print(f'Reporting on directory {dirname}')
+        for filename in os.listdir(dirname):
+            if not filename.endswith('.ckl'):
+                print(f'Skipping file {filename}')
+                continue
+            filtered = get_filtered_vulns(f'{dirname}/{filename}', filter)
+            if filtered:
+                vulns.extend(filtered)
+    else:
+        vulns = get_filtered_vulns(f'{dirname}/{filename}', filter)
+    
+    if len(vulns) == 0:
+        print('No vulnerabilities found')
+        return
+    write_csv(sorted(vulns))
+    print('Done')
+
+
+report('../ckl', [is_open])
